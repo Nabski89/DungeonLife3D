3e9528cbfb684e550113b4a1d2fc93b993844d31
Nabski89 <Nabski@Hotmail.com>
2023-08-12 09:25:56 -0400

    Customization, movement, party selection start

---
+++ Assets/DungeonClear.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonClear : MonoBehaviour
+{
+//reset all the checkpoints and spawn all the enemies
+}
